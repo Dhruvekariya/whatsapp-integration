7be35df78cfe4ffdf07f8a6f6083a05d60b53874
ShreyPragada <120622791+shreypragada131@users.noreply.github.com>
2025-03-12 12:46:25 +0530

    enhance chat response formatting to include original chat data

--- nodejs/server.js
+++ nodejs/server.js
@@ -196,6 +196,7 @@ app.get("/get-chats", async (_req, res) => {
         
         // Simplify the response to reduce processing time
         const formattedChats = chats.map(chat => ({
+            ...chat,
             id: chat.id._serialized,
             name: chat.name || chat.id._serialized,
             isGroup: chat.isGroup || false,
